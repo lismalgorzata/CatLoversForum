6ea45f1942cd17e69d3a619c730ef37ea8c2abe3
JuliaMekarska <s221455@student.uek.krakow.pl>
2024-05-04 22:29:40 +0200

    Home page carousel & authors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,57 @@
 <template>
-  <div id="carouselExample" class="carousel slide" style="background-color: #DDC7AB">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-<!--        <img src="..." class="d-block w-100" alt="...">-->
-        <h1 style="color: #E08989">Something here</h1>
+  <div class="container">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="../assets/home-carousel/Welcome.png"
+               alt="Welcome" style="width:100%;">
+        </div>
+
+        <div class="item">
+          <img src="../assets/home-carousel/Cafe.png"
+               alt="Cafe" style="width:100%;">
+        </div>
+
+        <div class="item">
+          <img src="../assets/home-carousel/Show.png"
+               alt="Show" style="width:100%;">
+        </div>
       </div>
-      <div class="carousel-item">
-<!--        <img src="..." class="d-block w-100" alt="...">-->
-        <h1 style="color: #E08989">Something here</h1>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+  <div class="container mt-5 text-center">
+    <h1>Authors</h1>
+    <div class="row">
+      <div class="col-sm-4 text-center">
+        <img src="../assets/authors/Gosia.jpg"
+             alt="Małgorzata Lis" style="width:200px; border-radius: 100%">
+        <h3 class="names">Małgorzata Lis</h3>
       </div>
-      <div class="carousel-item">
-<!--        <img src="..." class="d-block w-100" alt="...">-->
-        <h1 style="color: #E08989">Something here</h1>
+      <div class="col-sm-4 text-center">
+        <img src="../assets/authors/Malwina.jpg"
+             alt="Malwina Kochman" style="width:200px; border-radius: 100%">
+        <h3 class="names">Malwina Kochman</h3>
+      </div>
+      <div class="col-sm-4 text-center">
+        <img src="../assets/authors/Julka.jpg"
+             alt="Julia Mękarska" style="width:200px; border-radius: 100%">
+        <h3 class="names">Julia Mękarska</h3>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
   </div>
 </template>
 
@@ -32,4 +62,12 @@ export default {
 </script>
 
 <style>
+  .row {
+    margin-top: 50px;
+  }
+
+  .names {
+    margin-top: 20px;
+    margin-bottom: 40px;
+  }
 </style>
