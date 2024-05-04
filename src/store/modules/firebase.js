81161d7a22c96bf9ccb47cf791561a1dbfe8f7de
Małgorzata Lis <s222287@student.uek.krakow.pl>
2024-05-04 18:48:03 +0200

    firebase manual update

--- src/store/modules/firebase.js
+++ src/store/modules/firebase.js
@@ -1,4 +1,4 @@
-import { getAuth, onAuthStateChanged, updatePassword } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, updatePassword } from 'firebase/auth'
 import {
     collection,
     getDocs,
@@ -68,29 +68,43 @@ const mutations = {
 
 const actions = {
     [actionTypes.getPostsByUserId] (context, { uid }) {
-        context.commit(mutationType.addPostStart);
-        const q = uid ? query(collection(db, 'posts'), where('uid', '==', uid), orderBy('created', 'desc')) : query(collection(db, 'posts'), orderBy('created', 'desc'));
-        getDocs(q).then((result) => {
-            const posts = result.docs.map((doc) => ({
-                id: doc.id,
-                data: doc.data()
-            }));
-            context.commit(mutationType.setPosts, posts);
-        });
+        return new Promise((resolve) => {
+            context.commit(mutationType.addPostStart)
+            let q = query(collection(db, 'posts'), orderBy('created', 'desc'))
+            if (uid) {
+                q = query(
+                    collection(db, 'posts'),
+                    where('uid', '==', uid),
+                    orderBy('created', 'desc')
+                )
+            }
+
+            getDocs(q).then((result) => {
+                const posts = result.docs.map((doc) => {
+                    doc.data()
+                    return {
+                        id: doc.id,
+                        data: doc.data()
+                    }
+                })
+                context.commit(mutationType.setPosts, posts)
+                resolve()
+            })
+        })
     },
     [actionTypes.addPost] (context, data) {
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
+        return new Promise((resolve) => {
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user) => {
                 addDoc(collection(db, 'posts'), {
                     data,
                     uid: user.uid,
                     created: serverTimestamp()
-                }).then(() => {
-                    context.commit(mutationType.addPostSuccess);
-                });
-            }
-        });
+                })
+                context.commit(mutationType.addPostSuccess)
+                resolve()
+            })
+        })
     },
     [actionTypes.addComment] (context, { postId, comment }) { 
         console.log("addCommentIsFiring");
@@ -139,15 +153,102 @@ const actions = {
         }
     },
     [actionTypes.updatePassword] (context, { newPassword }) {
-        const auth = getAuth();
-        const user = auth.currentUser;
-        if (user) {
-            updatePassword(user, newPassword).then(() => {
-                context.commit('Password updated successfully');
-            }).catch((error) => {
-                console.error('Error updating password:', error);
+        return new Promise((resolve, reject) => {
+            const auth = getAuth()
+            const user = auth.currentUser
+            if (user) {
+                updatePassword(user, newPassword)
+                    .then(() => {
+                        resolve('Password updated successfully')
+                    })
+                    .catch((error) => {
+                        reject(error)
+                    })
+            } else {
+                // eslint-disable-next-line prefer-promise-reject-errors
+                reject('No authenticated user')
+            }
+        })
+    },
+    [actionTypes.getLikesForPosts] (context) {
+        return new Promise((resolve) => {
+            const q = query(collection(db, 'posts'), orderBy('created', 'desc'));
+
+            getDocs(q).then((result) => {
+                const likes = result.docs.map(doc => {
+                    const data = doc.data();
+                    return {
+                        id: doc.id,
+                        likes: data.likes || 0
+                    };
+                });
+                context.commit(mutationType.setLikes, likes);
+                resolve(likes);
+            }).catch(error => {
+                console.error("Error getting likes for posts:", error);
+                resolve([]);
             });
-        }
+        });
+    },
+    [actionTypes.incrementLikes] (context, { postId, userId }) {
+        const postRef = doc(db, 'posts', postId);
+        const userLikesRef = collection(db, 'userLikes');
+
+        return new Promise((resolve, reject) => {
+            updateDoc(postRef, {
+                likes: increment(1)
+            }).then(() => {
+                console.log('Likes incremented successfully.');
+
+                return addDoc(userLikesRef, {
+                    postId: postId,
+                    userId: userId,
+                    date: new Date()
+                });
+            }).then(() => {
+                console.log('User like added successfully.');
+                resolve();
+            }).catch(error => {
+                console.error("Failed to increment likes or add user like:", error);
+                reject(error);
+            });
+        });
+    },
+    [actionTypes.decrementLikes] (context, { postId, userId }) {
+        const postRef = doc(db, 'posts', postId);
+        const userLikesRef = collection(db, 'userLikes');
+        const q = query(userLikesRef, where("postId", "==", postId), where("userId", "==", userId));
+
+        return new Promise((resolve, reject) => {
+            getDocs(q).then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    deleteDoc(doc.ref);
+                });
+
+                updateDoc(postRef, {
+                    likes: increment(-1)
+                }).then(() => {
+                    console.log('Likes decremented successfully.');
+                    resolve();
+                }).catch(error => {
+                    console.error("Failed to decrement likes:", error);
+                    reject(error);
+                });
+            }).catch(error => {
+                console.error("Failed to find user like document:", error);
+                reject(error);
+            });
+        });
+    },
+    [actionTypes.checkUserLike] (context, { postId, userId }) {
+        const likesRef = collection(db, 'userLikes');
+        const q = query(likesRef, where("postId", "==", postId), where("userId", "==", userId));
+        return getDocs(q).then(querySnapshot => {
+            return querySnapshot.size > 0;
+        }).catch(error => {
+            console.error("Failed to check user likes:", error);
+            return false;
+        });
     },
     [actionTypes.getLikesForPosts] (context) {
         return new Promise((resolve) => {
