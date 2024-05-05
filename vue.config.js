55eb7b004a594aa7bb6e00d1c6908e3b64ced3ff
malwinakochman <malwinakochman@gmail.com>
2024-05-05 12:28:06 +0200

    Revert "PWA set up (pls work)"
    This reverts commit 1c2772d0fe6f2b92ef802ed57a062071531650b7.

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,4 @@
-const { defineConfig } = require('@vue/cli-service');
-
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  pwa: {
-    name: 'Cat Lovers Forum',
-    themeColor: '#000000',
-    msTileColor: '#ffffff',
-    manifestOptions: {
-      icons: [
-        {
-          src: 'src/assets/favicon.ico',
-          sizes: '64x64 32x32 24x24 16x16',
-          type: 'image/x-icon'
-        },
-        {
-          src: 'src/assets/cat192.png',
-          type: 'image/png',
-          sizes: '192x192'
-        },
-        {
-          src: 'src/assets/cat512.png',
-          type: 'image/png',
-          sizes: '512x512'
-        }
-      ]
-    }
-  }
-});
+  transpileDependencies: true
+})
